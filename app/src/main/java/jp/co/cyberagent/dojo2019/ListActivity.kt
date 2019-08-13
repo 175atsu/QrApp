0388d4ad0d1639f18fdd784351abf433192aa2e6
Atsu <zatsuyou0@gmail.com>
2019-08-13 10:27:02 +0900

    adapterのエラー解消

--- app/src/main/java/jp/co/cyberagent/dojo2019/ListActivity.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2019/ListActivity.kt
@@ -4,20 +4,35 @@ package jp.co.cyberagent.dojo2019
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.rxkotlin.toObservable
 import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.list_view.*
 import kotlin.concurrent.thread
 import java.util.Random
 
 class ListActivity  : AppCompatActivity() {
+
+//    private val mSampleData = itemList()
+//    private lateinit var adapter : RecyclerView.Adapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.list_view)
+
+        // データを設定
+//        adapter = RecyclerAdapter(this, mSampleData)
+//        listView2.adapter = adapter
+
+        recyclerViewInitialSetting()
 
         val buttonQr = findViewById<Button>(R.id.btnReturn)
         buttonQr.setOnClickListener {
@@ -67,25 +82,57 @@ class ListActivity  : AppCompatActivity() {
                 //データの流れを監視、みる、流れてくるたびにプリントを実行する　/DISPOSEというクラスの処理だから出てきた。
                 .subscribe({  Log.d("TAG6", it.uid.toString()+it.name)})
                 }
-//
-//        val data = ArrayList()
-//        data.add("国語")
-//        data.add("社会")
-//        data.add("算数")
-//        data.add("理科")
-//        data.add("生活")
-//        data.add("音楽")
-//        data.add("図画工作")
-//        data.add("家庭")
-//        data.add("体育")
-//
-//        // リスト項目とListViewを対応付けるArrayAdapterを用意する
-//        ArrayAdapter() adapter = new ArrayAdapter<>(this, android.R.layout.list_view, data);
-//
-//        // ListViewにArrayAdapterを設定する
-//        ListView listView = (ListView)findViewById(R.id.memberList);
-//        listView.setAdapter(adapter);
+
    }
+
+
+    //アダプターなどなどの設定
+    fun recyclerViewInitialSetting() {
+        val recyclerView = recyclerView
+        val adapter = MemberRecycleViewAdapter(fetchAllUserData(), object : MemberRecycleViewAdapter.ListListener {
+            override fun onClickRow(tappedView: View, userListModel: MemberListModel) {
+                this.onClickRow(tappedView, userListModel)
+            }
+        })
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+
+    }
+
+    //セルのタップ
+//    fun onClickRow(tappedView: View, userListModel: MemberListModel) {
+//        //toDetailViewへ画面遷移
+//        toUserDetailActivity(userListModel)
+//    }
+
+    //DBに保存したデータの取得
+    fun fetchAllUserData(): List<MemberListModel> {
+        //データリスト
+        val dataList = mutableListOf<MemberListModel>()
+        //DBからユーザーデータの取得
+        var memberData  = Single.fromCallable { AppDatabase.getInstance(this).userDao().getAll() } //データベースから取ってくる
+            //ioスレッドで実行する。
+            .subscribeOn(Schedulers.io())
+            //シングルからオブザバブルに変換
+            .flatMapObservable { it.toObservable() }
+            //変換する　文字列で来たものを数字にしたり
+            .map { it}
+            //ここまでに処理をmainThreadで実行 デフォルトの名前
+            .observeOn(AndroidSchedulers.mainThread())
+            //データの流れを監視、みる、流れてくるたびにプリントを実行する　/DISPOSEというクラスの処理だから出てきた。
+            .subscribe({Log.d("TAG12", it.name.toString())
+                var data : MemberListModel = MemberListModel().also {
+                    it.nameID = it.nameID
+                    it.twitterID = it.twitterID
+                    it.githubID = it.githubID
+                    }
+                dataList.add(data)})
+
+        return dataList
+    }
+
+
 
     override fun onStart() {
         super.onStart()
--- app/src/main/java/jp/co/cyberagent/dojo2019/MemberViewHolder.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2019/MemberViewHolder.kt
@@ -5,18 +5,13 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 
-public class MemberViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class MemberViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     interface ItemClickListener {
         fun onItemClick(view: View, position: Int)
     }
-    var nameID: TextView? = null
-    var twitterID: TextView? = null
-    var githubID: TextView? = null
-    fun MemberViewHolder(itemView: View) {
-        super.itemView
-        nameID = itemView.findViewById(R.id.name)
-        twitterID = itemView.findViewById(R.id.twitter)
-        githubID = itemView.findViewById(R.id.github)
-    }
+
+    val nameID: TextView = itemView.findViewById(R.id.name)
+    val twitterID: TextView = itemView.findViewById(R.id.twitter)
+    val githubID: TextView = itemView.findViewById(R.id.github)
 }
