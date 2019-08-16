97c55c68022f23013b2870a34004bc0c49b39b09
Atsu <zatsuyou0@gmail.com>
2019-08-16 13:26:41 +0900

    var3

--- app/src/main/java/jp/co/cyberagent/dojo2019/MemberDetailActivity.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2019/MemberDetailActivity.kt
@@ -27,11 +27,11 @@ class MemberDetailActivity : AppCompatActivity() {
         web()
         showAccount()
 
-        val buttonReturn = findViewById<Button>(R.id.btnReturn)
-        buttonReturn.setOnClickListener {
-            intent = Intent(this, ListActivity::class.java)
-            startActivity(intent);
-        }
+//        val buttonReturn = findViewById<Button>(R.id.btnReturn)
+//        buttonReturn.setOnClickListener {
+//            intent = Intent(this, ListActivity::class.java)
+//            startActivity(intent);
+//        }
 
         val buttonDetail = findViewById<Button>(R.id.btnDetail)
         buttonDetail.setOnClickListener {
@@ -58,9 +58,9 @@ class MemberDetailActivity : AppCompatActivity() {
     //アラート
     // ダイアログを作成して表示
     fun alert() = AlertDialog.Builder(this).apply {
-        setTitle("削除します")
-        setMessage("削除してもよろしいですか")
-        setPositiveButton("OK", DialogInterface.OnClickListener { _, _ ->
+        setTitle("削除できません")
+        setMessage("１度出会った人との繋がりを絶っていいんですか？")
+        setPositiveButton("NO", DialogInterface.OnClickListener { _, _ ->
             // OKをタップしたときの処理
             // 永続データベースを作成
             val db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "database.db").build()
@@ -79,7 +79,7 @@ class MemberDetailActivity : AppCompatActivity() {
 //                }
 //            }
         })
-        setNegativeButton("Cancel", null)
+        setNegativeButton("NO", null)
         show()
     }
 
